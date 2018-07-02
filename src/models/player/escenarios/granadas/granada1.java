63ded3473075739eff2b02ceaf2b466efd8d7cbd
luist23 <00127717@uca.edu.sv>
2018-07-01 20:42:33 -0600

    elaboracion de logica en grandas... se modificaron funciones de la clase gravedad para ser aprobechadas por otras clases

--- src/models/player/escenarios/granadas/granada1.java
+++ src/models/player/escenarios/granadas/granada1.java
@@ -1,5 +1,73 @@
 package models.player.escenarios.granadas;
 
-public class granada1 {
-    public static String[] grandaEfecto;
+import javafx.application.Platform;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import models.player.GameValues;
+import models.player.PlayerRick.Gravedad;
+import models.player.PlayerRick.Player;
+import models.player.peldannos.Peldanno;
+
+public class granada1 implements granada <granadaUno>{
+    public static String[] granadaEfecto;
+
+    @Override
+    public void activar() {
+        granada.setImage(new Image(this.getClass().getResource(granadaEfecto[0]).toExternalForm()));
+        Thread uno = new Thread(()->{
+
+            for (int i=1 ; i<15;i++){
+                //GameValues.permitirSalto=false;
+
+                Platform.runLater(() -> granada.setLayoutY(granada.getLayoutY() - 15));
+                Platform.runLater(() -> granada.setLayoutY(granada.getLayoutY() + 5));
+                Gravedad.sleeping(25);
+                //}
+            }//GameValues.gravedad=true;
+            for (int i=1 ; i<15;i++){
+                //GameValues.permitirSalto=false;
+
+                //Platform.runLater(() -> granada.setLayoutY(granada.getLayoutY() - 15));
+                Platform.runLater(() -> granada.setLayoutY(granada.getLayoutY() + 5));
+                Gravedad.sleeping(25);
+                //}
+            }
+            while(Gravedad.efectoGravedad(granada)){
+                if((GameValues.dimension[1] - granadaUno.dimension[0]) > granada.getLayoutY()){
+                    Platform.runLater(() -> granada.setLayoutY(granada.getLayoutY() +
+                            5));
+
+                }else {
+                    break;
+                }
+
+
+            Gravedad.sleeping(25);
+
+
+
+        }
+
+
+
+            Gravedad.sleeping(2500);
+            granada.setImage(new Image(this.getClass().getResource(granadaEfecto[1]).toExternalForm()));
+
+            //g.setPosicionFinal(posicionFinal);
+
+
+            //imageView.setImage(new Image(values.getClass().getResource(posicionFinal[0]).toExternalForm()));
+
+        });
+        uno.setDaemon(true);
+        //thread.start();
+        uno.start();
+    }
+
+    @Override
+    public ImageView getGranada() {
+        return null;
+    }
+
+
 }
---
+++ src/models/player/escenarios/granadas/granadaUno.java
@@ -0,0 +1,11 @@
+package models.player.escenarios.granadas;
+
+public class granadaUno {
+    public static String[] fases;
+    public static int[] dimension;
+
+
+
+
+
+}
